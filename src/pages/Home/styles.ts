d686b549d8717479d854825e427b69bff0a717c2
Wagner Nascimento <wagneer_nascimento@live.com>
2022-10-22 10:27:08 -0300

    styles home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,12 +6,7 @@ export const Container = styled.SafeAreaView`
    background: ${COLORS.WHITE};
 `;
  
-export const ContainerBanner = styled.View`
-    width: 100%;
-    height: 60px;
-    align-items: center;
-    justify-content: center;
-`;
+
 export const ContainerInput = styled.View`
    padding: 20px;
 `;
